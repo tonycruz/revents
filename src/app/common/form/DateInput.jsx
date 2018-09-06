5c26a43ef39633f12b92041d023ed4f4131783c9
Antonio Lopes da Cruz <tonycruz@outlook.com>
2018-09-06 14:46:53 +0100

    Enf of section 16

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -4,8 +4,12 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import moment from 'moment'
 
-const DateInput = ({ input: { value, onChange, ...restInput }, width, placeholder, meta: { touched, error }, ...rest }) => {
+const DateInput = ({ input: { value, onChange, onBlur, ...restInput }, width, placeholder, meta: { touched, error }, ...rest }) => {
+    if (value) {
+        value = moment(value, 'X')
+    }
     return (
+
         <Form.Field error={touched && !!error} width={width}>
             <DatePicker
                 {...rest}
@@ -20,3 +24,4 @@ const DateInput = ({ input: { value, onChange, ...restInput }, width, placeholde
 }
 
 export default DateInput
+
